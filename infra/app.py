725e366d7bef64a2883ce8c62e9cfa9c800b7009
Ananth Raghavendra <raganant@amazon.com>
2023-09-01 09:23:52 -0700

    fix

--- infra/app.py
+++ infra/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+
 import os
 import aws_cdk as cdk
 
 from pipeline.infra_stack import PipelineStack
-from ecs_fargate.infra_stack import ECSStack
+from ecs_fargate.infra_stack import ECSFargateStack
 from vpc_base.infra_stack import VPCStack
 
 class GravitonSpotWorkshop(cdk.App):
@@ -11,9 +12,9 @@ class GravitonSpotWorkshop(cdk.App):
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
             self.stack_name = "GravitonSpotWorkshop"
-            self.base_module = VPCStack(self, self.stack_name + "-base")
+            self.base_module = VPCStack(self, self.stack_name + "-vpc")
             self.pipeline_module = PipelineStack(self, self.stack_name + "-pipeline")
-            self.ecs_fargate_module = ECSStack(self, self.stack_name + "-ecs-fargate")
+            self.ecs_fargate_module = ECSFargateStack(self, self.stack_name + "-ecs-fargate",self.base_module.vpc)
 
 if __name__ == '__main__':
     app = GravitonSpotWorkshop()
